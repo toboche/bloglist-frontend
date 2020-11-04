dc50f13aab20551c45490a87337a74cda88936af
toboche <toboche@gmail.com>
2020-11-04 20:18:37 +0100

    5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('renders content in default state', () => {
@@ -29,4 +29,55 @@ test('renders content in default state', () => {
     expect(component.container)
         .not
         .toHaveTextContent('poster')
+})
+
+test('renders all content in the expanded state', () => {
+    const blog = {
+        title: 'boring blog',
+        author: 'nobody',
+        url: 'www.www.ww',
+        likes: 1,
+        user: { name:'poster' }
+    }
+
+    const component = render(
+        <Blog blog={blog} />
+    )
+
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    expect(component.container)
+        .toHaveTextContent('boring blog')
+    expect(component.container)
+        .toHaveTextContent('www.www.ww')
+    expect(component.container)
+        .toHaveTextContent('1')
+    expect(component.container)
+        .toHaveTextContent('poster')
+})
+
+test('clicking like triggers handler', () => {
+    const blog = {
+        title: 'boring blog',
+        author: 'nobody',
+        url: 'www.www.ww',
+        likes: 1,
+        user: { name:'poster' }
+    }
+
+    const mockHandleLikeClicked = jest.fn()
+
+    const component = render(
+        <Blog blog={blog} handleLikeClicked={mockHandleLikeClicked}/>
+    )
+
+    const button = component.getByText('view')
+    fireEvent.click(button)
+    const likeButton = component.getByText('like')
+    fireEvent.click(likeButton)
+    fireEvent.click(likeButton)
+
+    expect(mockHandleLikeClicked.mock.calls)
+        .toHaveLength(2)
 })
