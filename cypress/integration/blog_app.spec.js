32886adce4b4a7b84b4a1c5c4dd42a9c2f6ab0f2
toboche <toboche@gmail.com>
2020-11-05 21:48:47 +0100

    5.18

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -2,6 +2,12 @@ describe('Blog app', function() {
 
     beforeEach(function() {
         cy.request('POST', 'http://localhost:3001/api/testing/reset')
+        const user = {
+          name: 'Matti Luukkainen',
+          username: 'mluukkai',
+          password: 'salainen'
+        }
+        cy.request('POST', 'http://localhost:3001/api/users/', user)         
         cy.visit('http://localhost:3000')
     })
 
@@ -11,5 +17,21 @@ describe('Blog app', function() {
       cy.contains('username')
       cy.contains('password')
       cy.contains('login')
+    })
+
+    describe('Login',function() {
+        it('succeeds with correct credentials', function() {
+            cy.get('#username').type('mluukkai')
+            cy.get('#password').type('salainen')
+            cy.get('#signin').click()
+            cy.contains('mluukkai is logged in')
+        })
+
+        it('fails with wrong credentials', function() {
+            cy.get('#username').type('mluukkai')
+            cy.get('#password').type('incorrectpass')
+            cy.get('#signin').click()
+            cy.contains('wrong credentials')
+        })
     })
   })
--- src/App.js
+++ src/App.js
@@ -53,6 +53,7 @@ const App = () => {
               <div>
                 username
                   <input
+                  id="username"
                   type="text"
                   value={username}
                   name="Username"
@@ -62,13 +63,14 @@ const App = () => {
               <div>
                 password
                   <input
+                  id="password"
                   type="password"
                   value={password}
                   name="Password"
                   onChange={({ target }) => setPassword(target.value)}
                 />
               </div>
-              <button type="submit">login</button>
+              <button id="signin" type="submit">login</button>
             </form>
           </div>
   }
